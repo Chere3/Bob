73a17074dc64a88799c35f3973178b6ac997a2f3
Cheree <71246795+Chere3@users.noreply.github.com>
2022-02-19 14:55:25 -0600

    agregado el test mode de nuevo a donde corresponde.

--- src/Events/message/messageDelete.ts
+++ src/Events/message/messageDelete.ts
@@ -2,6 +2,7 @@ import { Client, Message } from 'discord.js';
 import { getTestMode } from '../../Util/managers/littleManagers/cacheManager';
 import { snipeCore } from '../../Util/managers/littleManagers/snipeManager';
 export const run =async  (bot: Client, message: Message) => {
+    if (getTestMode() == true) return;
     if (message.partial) await message.fetch();
     if (message.author.bot) return;
     if (message.channel.type === 'DM') return;
--- src/Events/message/messageUpdate.ts
+++ src/Events/message/messageUpdate.ts
@@ -2,6 +2,7 @@ import { Client, Message } from 'discord.js';
 import { getTestMode } from '../../Util/managers/littleManagers/cacheManager';
 import { editSnipeCore } from '../../Util/managers/littleManagers/editSnipeManager';
 export const run = async (client: Client, message: Message, editedMessage: Message) => {
+    if (getTestMode() == true) return;
     if (message.partial) message.fetch();
     if (message.author.bot) return;
     if (getTestMode() == true) return;
